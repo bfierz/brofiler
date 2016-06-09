1f4d87787a6f6b99501d848af22a7756e21913fb
Basil Fierz <basil.fierz@hotmail.com>
2016-06-09 20:31:58 +0200

    Cosmetic changes

--- ProfilerCore/Event.cpp
+++ ProfilerCore/Event.cpp
@@ -56,7 +56,7 @@ void Event::Stop(EventData& data)
 OutputDataStream & operator<<(OutputDataStream &stream, const EventDescription &ob)
 {
 	byte flags = (ob.isSampling ? 0x1 : 0);
-	return stream << ob.name << ob.file << ob.line << (unsigned long) ob.color << flags;
+	return stream << ob.name << ob.file << ob.line << (uint32_t) ob.color << flags;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 OutputDataStream& operator<<(OutputDataStream& stream, const EventTime& ob)
